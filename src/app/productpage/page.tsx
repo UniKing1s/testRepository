8ad385be296748546cd1c3af00485604ff8e6a87
UniKing1s <khoivo050603it@gmail.com>
2024-06-09 00:49:57 +0700

    commit first time

---
+++ src/app/productpage/page.tsx
@@ -0,0 +1,10 @@
+const Page = () => {
+
+    return (
+        <div className="flex justify-center items-center font-bold h-screen w-full">
+            sản phẩm đầu ra
+        </div>
+    );
+}
+
+export default Page;
