f1cf6bc7b54b73349d234b0b3c3e0219199eeed1
UniKing1s <khoivo050603it@gmail.com>
2024-06-09 00:49:57 +0700

    commit first time

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,8 @@
 'use client'
 import { categoryApi } from "@/until/apiService";
 import { useEffect, useState } from "react";
-interface Category{
-  name: string,
-  configurations: string[]
-}
 export default function Home() {
-  const [data, setData] = useState<Category[]>([]);
-  useEffect(() => {
-    categoryApi("/","GET",{}).then((res:any)=>{
-      if (res.status === 200) {
-        setData(res.data.categories);
-      }
-    })
-  }, []);
   return (
-    <div>{data.map((item)=>{return <div key={item.name}>{item.name}</div>})}</div>
+    <div>HI</div>
   );
 }
--- src/until/apiService.tsx
+++ src/until/apiService.tsx
@@ -9,3 +9,12 @@ export function categoryApi(endpoint:string,method:string,body:object){
         console.log(error);
       });
 }
+export function productApi(endpoint:string,method:string,body:object){
+    return axios({
+        method: method,
+        url: process.env.NEXT_PUBLIC_API +"/products"+ endpoint,
+        data: body
+    }).catch((error) => {
+        console.log(error);
+      });
+}
